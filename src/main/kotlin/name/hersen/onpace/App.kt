dfb5d20ce44b0c7cb406c99ca0e7bf176e84ac6b
Rudolf Hersén <rudolf.hersen@schibsted.com>
2020-05-30 21:47:18 +0200

    show target distance

--- src/main/kotlin/name/hersen/onpace/App.kt
+++ src/main/kotlin/name/hersen/onpace/App.kt
@@ -9,57 +9,48 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import java.time.LocalDate
 
 fun main() {
     JavalinApp(7000).init()
 }
 
 class JavalinApp(private val port: Int) {
-
     fun init(): Javalin {
-
-        // get heroku port or uses default (for local environment)
-        val port: Int = System.getenv("PORT")?.toIntOrNull() ?: port
-
-        // starts Javalin
         val app = Javalin.create().apply {
             exception(Exception::class.java) { e, _ -> e.printStackTrace() }
             error(404) { ctx -> ctx.result("Not found") }
-        }.start(port)
+        }.start(System.getenv("PORT")?.toIntOrNull() ?: this.port)
 
-        // app endpoints
         app.routes {
             get("/") { ctx ->
+                val url = URL("https://www.strava.com/api/v3/athletes/13317026/stats")
+                val con: HttpURLConnection = url.openConnection() as HttpURLConnection
                 try {
-                    ctx.result(athlete().ytd_run_totals.distance.toString())
+                    val distance: Double? = athlete(con)?.ytd_run_totals?.distance
+                    val target = 15e5 * LocalDate.now().dayOfYear / 366.0
+                    ctx.result(distance.toString() + " " + target)
                 } catch (e: Exception) {
-                    ctx.status(400).result(e.message ?: e.toString())
+                    ctx.status(con.responseCode).result(e.message ?: e.toString())
                 }
-
             }
         }
 
         return app
     }
 
-    private fun athlete(): RootObject {
-        val url = URL("https://www.strava.com/api/v3/athletes/13317026/stats")
-        val con: HttpURLConnection = url.openConnection() as HttpURLConnection
-        try {
-            con.setRequestProperty("Authorization", "Bearer " + System.getenv("BEARER"))
-            val content = StringBuilder()
-            BufferedReader(InputStreamReader(con.inputStream)).use { reader ->
-                var line: String? = reader.readLine()
-                while (line != null) {
-                    content.append(line)
-                    line = reader.readLine()
-                }
-                val mapper = ObjectMapper().registerModule(KotlinModule())
-                JavalinJackson.configure(mapper)
-                return mapper.readValue(content.toString(), RootObject::class.java)
+    private fun athlete(con: HttpURLConnection): RootObject? {
+        con.setRequestProperty("Authorization", "Bearer " + System.getenv("BEARER"))
+        val content = StringBuilder()
+        BufferedReader(InputStreamReader(con.inputStream)).use { reader ->
+            var line: String? = reader.readLine()
+            while (line != null) {
+                content.append(line)
+                line = reader.readLine()
             }
-        } finally {
-            println(con.responseCode)
+            val mapper = ObjectMapper().registerModule(KotlinModule())
+            JavalinJackson.configure(mapper)
+            return mapper.readValue(content.toString(), RootObject::class.java)
         }
     }
 }
