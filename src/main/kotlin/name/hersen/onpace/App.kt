04a823e0466e8d7b012387436ca521ef9fc69a6a
Rudolf Hersén <rudolf.hersen@schibsted.com>
2020-05-31 22:31:54 +0200

    khttp

--- src/main/kotlin/name/hersen/onpace/App.kt
+++ src/main/kotlin/name/hersen/onpace/App.kt
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.plugin.json.JavalinJackson
+import khttp.responses.Response
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.URLEncoder
 import java.time.LocalDate
 
 fun main() {
@@ -24,43 +26,25 @@ class JavalinApp(private val port: Int) {
 
         app.routes {
             get("/") { ctx ->
-                val url = URL("https://www.strava.com/api/v3/athletes/13317026/stats")
-                val con: HttpURLConnection = url.openConnection() as HttpURLConnection
-                try {
-                    val distance: Double? = athlete(con)?.ytd_run_totals?.distance
-                    if (distance == null) {
-                        ctx.result("ytd_run_totals saknas")
-                    } else {
-                        val target: Double = 15e5 * LocalDate.now().dayOfYear / 366.0
-                        ctx.html(
-                            """<html>
+                val response: Response = khttp.get(
+                    "https://www.strava.com/api/v3/athletes/13317026/stats",
+                    headers = mapOf("Authorization" to "Bearer ${System.getenv("BEARER")}")
+                )
+                val mapper = ObjectMapper().registerModule(KotlinModule())
+                JavalinJackson.configure(mapper)
+                val rootObject: RootObject = mapper.readValue(response.text, RootObject::class.java)
+                val distance: Double = rootObject.ytd_run_totals.distance
+                val target: Double = 15e5 * LocalDate.now().dayOfYear / 366.0
+                ctx.html(
+                    """<html>
 <meta charset='UTF-8'>
 <div>Du har sprungit ${String.format("%.1f", distance / 100)} km i år.</div>
 <div>Målet är ${String.format("%.1f", target / 100)} km.</div>
 <div>Du ligger ${String.format("%.1f", target - distance)} meter efter.</div>"""
-                        )
-                    }
-                } catch (e: Exception) {
-                    ctx.status(con.responseCode).result(e.message ?: e.toString())
-                }
+                )
             }
         }
 
         return app
-    }
-
-    private fun athlete(con: HttpURLConnection): RootObject? {
-        con.setRequestProperty("Authorization", "Bearer " + System.getenv("BEARER"))
-        val content = StringBuilder()
-        BufferedReader(InputStreamReader(con.inputStream)).use { reader ->
-            var line: String? = reader.readLine()
-            while (line != null) {
-                content.append(line)
-                line = reader.readLine()
-            }
-            val mapper = ObjectMapper().registerModule(KotlinModule())
-            JavalinJackson.configure(mapper)
-            return mapper.readValue(content.toString(), RootObject::class.java)
-        }
     }
 }
